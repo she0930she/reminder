3712c931704b46a10a01329228839b43cec00f15
she0930she <she0930she@hotmail.com>
2020-12-22 14:49:24 +0800

    deploy

---
+++ pages/createCustomer.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <!-- TODO: -->
+    <!-- navbar -->
+    <!-- navbar END-->
+    <h1>createCustomer</h1>
+    <form @submit.prevent="createCustomer">
+      <label for="">舊客戶姓名:</label>
+      <input type="text" placeholder="舊客戶姓名" />
+      <label for="">新客戶姓名:</label>
+      <input type="text" placeholder="新客戶姓名" />
+      <br />
+      <label for="">購買日期: </label>
+      <input type="radio" />
+      <!--今天或 日曆給它選 -->
+      <input type="radio" />
+      <br />
+      <label for="">購買品項：</label>
+      <select name="" id="">
+        <option value="">保養品</option>
+        <option value="">機器</option>
+        <option value="">保健食品</option>
+        <option value="">TR90</option>
+        <option value="">輔銷品</option>
+      </select>
+      <label for="">數量</label>
+      <input type="number" />
+      <br />
+
+      <button>新增購買品項</button>
+      <br />
+      <br />
+      <label for="">Note</label>
+      <input type="text" placeholder="備註" />
+      <br />
+      <br />
+      <button>新增客戶資料</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "createCustomer",
+  data() {
+    return {};
+  },
+  methods: {
+    createCustomer() {
+      console.log("click");
+      const newShop = {
+        customerName: "",
+        shopDate: "",
+        shopItem1: "",
+        shopItem1N: "",
+        note: "",
+        createdAt: new Date().getTime(),
+      };
+      console.log("[newShop]", newShop);
+    },
+  },
+};
+</script>
---
+++ pages/customer.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <h1>customer page</h1>
+    <!-- TODO: -->
+    <!-- navbar -->
+    <!-- navbar END-->
+    <button @click="reminder" class="btn btn-secondary">本月提醒</button>
+    <br />
+    <br />
+    <form @submit.prevent="historyCustomer">
+      <h3>歷史資料</h3>
+      <label for="">客戶姓名</label>
+      <input v-model="customerName" type="text" placeholder="客戶姓名" />
+      <h4>或</h4>
+      <input v-model="customerTel" type="text" placeholder="客戶手機號碼" />
+      <!-- <h4>
+        或<br />
+        使用下拉選單尋找客戶
+      </h4> -->
+      <!-- <select v-model="historyName" id="">
+        <option name="historyName" value="" disabled>請選擇客戶姓名</option>
+        <option name="historyName" value="">大雄</option>
+        <option name="historyName" value="">靜香</option>
+        <option value="">胖虎</option>
+      </select> -->
+      <button>搜尋</button>
+    </form>
+    <br />
+    <br />
+    <button>使用產品搜尋客戶</button>
+    <br />
+    <br />
+    <button @click="newCumstomer">新增客戶資料</button>
+    <h3>本月提醒區:</h3>
+    <h3>客戶歷史資料區:</h3>
+    <p>{{ customerList }}</p>
+    <h3>新增客戶資料區:</h3>
+    <h3>產品搜尋客戶區:</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "customer",
+  data() {
+    return {
+      customerName: "",
+      customerTel: "",
+      customerList: [],
+    };
+  },
+  methods: {
+    reminder() {
+      console.log("click");
+    },
+    newCumstomer() {
+      console.log("click");
+    },
+    historyCustomer() {
+      console.log("click");
+      const historySearch = {
+        // return裡面的customerName{{}} 對上 this.customerName
+        customerName: this.customerName,
+        customerTel: this.customerTel,
+        createdAt: new Date().getTime(),
+      };
+      console.log("[historySearch]", historySearch);
+      this.customerList.push(historySearch);
+    },
+  },
+};
+</script>
